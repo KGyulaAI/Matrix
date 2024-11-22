25270673baa1d17f0e0cf40e4032225803f57b63
kovacs.gyula <kovacs.gyula@BURG-1E03-05>
2024-11-22 14:12:06 +0100

    Add project files.

---
+++ Matrix/Matrix.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Matrix
+{
+    internal class Matrix
+    {
+        double[,] matrix;
+
+        public Matrix()
+        {
+            matrix = new double[3, 3];
+        }
+        public Matrix(int n)
+        {
+            matrix = new double[n, n];
+        }
+        public Matrix(int m, int n)
+        {
+            matrix = new double[m, n];
+        }
+
+        public static Matrix Sum(Matrix A, Matrix B)
+        {
+            Matrix eredmeny = new Matrix(A.Sorok, A.Oszlopok);
+            for (int i = 0; i < A.Sorok; i++)
+            {
+                for (int j = 0; j < A.Oszlopok; j++)
+                {
+                    eredmeny.matrix[i, j] = A.matrix[i, j] + B.matrix[i, j];
+                }
+            }
+            return eredmeny;
+        }
+        public void Fill(double ertek)
+        {
+            for (int i = 0; i < Sorok; i++)
+            {
+                for (int j = 0; j < Oszlopok; j++)
+                {
+                    matrix[i, j] = ertek;
+                }
+            }
+        }
+
+        public int Oszlopok
+        {
+            get { return matrix.GetLength(1); }
+        }
+        public int Sorok
+        {
+            get { return matrix.GetLength(0); }
+        }
+    }
+}
---
+++ Matrix/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Matrix matrix = new Matrix();
+            Matrix matrix2 = new Matrix(5);
+            Matrix matrix3 = new Matrix(6, 7);
+
+            matrix.Fill(1);
+            matrix2.Fill(5);
+            matrix3.Fill(10);
+        }
+    }
+}
